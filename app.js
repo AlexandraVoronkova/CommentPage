d4048efeafdaef72e40da43fba1e17838c3e246f
Voronkova Alexandra <alvoronkova96@mail.ru>
2017-05-08 12:15:06 +0300

    test3

--- app.js
+++ app.js
@@ -1,36 +1,43 @@
 var express = require( 'express' );
-var bodyParser = require ( 'body-Parser' );
 
-var app = express();
+function startApp(){
+	var app = express();
+	var comments = require('./routes/comments.js');
+	app.use('./routes/comments.js', comments);
+	app.get('/comments', comments);
+	app.post('/comments', comments);
+	
+	app.listen(3000,function(){
+		console.log('Work on port : 3000');
+	});
+}
 
-app.use( bodyParser.urlencoded({extended: true}) );
-app.use( bodyParser.json());
+var MongoClient = require('mongodb').MongoClient;
+var Collection = require('mongodbext').Collection;
 
-var Comments=require('./models/comment.js').Comment;
-
-//Обработчик '/comment'
-app.get('/comment',function (req, res) {
-	Comments.find({}, function(err, comments){
-		if(err) {
-			res.send(err);
+MongoClient.connect('mongodb://localhost:27017/chat',function(err, db){
+		if (err) {
+			console.error(err);
+			return;
 		}
 		else{
-			res.render('index.ejs', {comments:comments});
+			collection = db.collection('comments');
+			startApp();
 		}
 	});
-});
-//обработчик получения данных
-app.post('/comment',function(req,res){
-	//получаем переменные с данными из формы /comment
-	var name=req.body.name; 
-	var comment=req.body.comment;
-	//добавление комментария в БД
-	var newComm = new Comments({name: name, comment:comment});
-	newComm.save();
-	//перейти обратно в корень сайта
-	res.redirect('/comment');
-});
 
-app.listen(3000,function(){
-	console.log('Work on port : 3000');
-});
+//var Step = require('twostep').Step;
+
+/*Step(
+	MongoClient.connect('mongodb://localhost:27017/chat'),
+	function(err, db){
+		if (err) {
+			console.error(err);
+			return;
+		}
+		else{
+			collection = db.collection('comments');
+			startApp();
+		}
+	}
+);*/
