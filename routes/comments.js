36f2ec46305e304251b712434add0b01658f8441
Voronkova Alexandra <alvoronkova96@mail.ru>
2017-09-22 16:28:30 +0300

    Steppy

--- routes/comments.js
+++ routes/comments.js
@@ -21,36 +21,36 @@ var schema = {
 
 module.exports = function(router) {
 	//Обработчик '/comments'
-	router.get('/comments', function(req, res, next) { 
-		Steppy(
-		function() {
-			db.comments.find({}).toArray(this.slot());
-		},
-		function(err, comments) {
-			res.render('index.jade', {comments: comments});
-			this.pass(null);
-		},
-		function(err) {
-			next();
+	router.get('/comments', 
+		function(req, res, next) { 
+			Steppy(
+				function() {
+					db.comments.find({}).toArray(this.slot());
+				},
+				function(err, comments) {
+					res.render('index.jade', {comments: comments});
+					this.pass(null);
+				},
+				next
+			);
 		}
-		);
-	});
+	);
 	
 	//обработчик получения данных
-	router.post('/comments', function(req, res, next) {
-		Steppy(
-			function() {
-				var params = req.validate(schema);
-				//добавление комментария в БД
-				db.comments.insertOne(params,this.slot());
-			},
-			function(err) {
-				res.redirect('/comments');
-				this.pass(null);
-			},
-			function(err) {
-				next();
-			}
+	router.post('/comments',
+		function(req, res, next) {
+			Steppy(
+				function() {
+					var params = req.validate(schema);
+					//добавление комментария в БД
+					db.comments.insertOne(params,this.slot());
+				},
+				function(err) {
+					res.redirect('/comments');
+					this.pass(null);
+				},
+				next
 			);
-	});
+		}
+	);
 }
